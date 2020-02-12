aa81109af247595639e79616ce6599c1bfdb0c85
Rattlecode17sh <lalit1701sh@gmail.com>
2020-02-13 00:33:00 +0530

    First Commit

--- admin_area/check_pass.php
+++ admin_area/check_pass.php
@@ -24,7 +24,41 @@
 ?>
 
 <!-- HTML for changing password for the first time -->
-  <h2>Change password</h2>
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>change password</title>
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+ </head>
+
+ <body>
+ <div class = "container">
+ <h1 class = "jumbotron text-center">Change Password</h1>
+ 
+  <form>
+  <div class="form-group">
+  
+    <label for="newpassword">New Password</label>
+    <input type="email" class="form-control" id="newpassword" aria-describedby="emailHelp" placeholder="Enter email" name = "pass">
+    
+  </div>
+  <div class="form-group">
+    <label for="retypepassword">Re-type Password</label>
+    <input type="password" class="form-control" id="retypepassword" placeholder="Password" name = "repass">
+  </div>
+  <div class="form-check">
+    
+  </div>
+  <br>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+</div>
+ </body>
+ </html> 
 
 
 <?php } }?>
