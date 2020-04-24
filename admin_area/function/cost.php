8f2dd333f9d89f3672f1f47ba4cc2ffd3733ea9c
Lalit Sharma <38866385+Rattlecode17sh@users.noreply.github.com>
2020-04-24 21:53:44 +0530

    update cost page

---
+++ admin_area/function/cost.php
@@ -0,0 +1,45 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!DOCTYPE html>
+<html lang="en">
+<style>
+.btn{
+    height: 35px;
+  width: 50%;
+}
+
+</style>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <!--Fontawesome CDN-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    
+</head>
+
+<body>
+    <div class="container">
+        <br>
+       <div class="d-flex flex-column bd-highlight mb-6">
+        <div class="btn-group-vertical">
+            <br>
+            <button type="button" class="btn btn-info ">2 Wheeler</button>
+            <br>
+            <button type="button" class="btn btn-info ">4 Wheeler</button>
+           
+          </div>
+        
+      </div>
+      <div class="d-flex flex-column-reverse bd-highlight">
+        
+      </div>
+    </div>
+     
+
+  
+    
+</body>
