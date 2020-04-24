fd64b500e842bb7b91e8629dad27ce4ac8442a85
Lalit Sharma <38866385+Rattlecode17sh@users.noreply.github.com>
2020-04-24 11:00:40 +0530

    Update kar lena
    Directly merge kar le

--- admin_area/admin_dashboard.php
+++ admin_area/admin_dashboard.php
@@ -1,13 +1,33 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 </head>
 <body>
-   
-  <h1>Welcome to Admin Dashboard</h1>
+    <div class = "container">  
+  <h1 class = "jumbotron text-center">Welcome to Admin Dashboard</h1>
+
+  <ul class="list-group">
+    <li class="list-group-item">Change password</li>
+    <li class="list-group-item">See status of parking lot</li>
+    <li class="list-group-item">See all details</li>
+    <li class="list-group-item">Current cost details</li>
+    <li class="list-group-item">Current Costing details</li>
+    <li class="list-group-item">Change 2 wheeler cost</li>
+    <li class="list-group-item">Change 4 wheeler cost</li>
+    <li class="list-group-item">Change 2 wheeler parking slots</li>
+    <li class="list-group-item">Change 4 wheeler parking cost</li>
+  </ul>
+
+  </div>
     
 </body>
-</html>
