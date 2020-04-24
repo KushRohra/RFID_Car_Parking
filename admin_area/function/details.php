500a6bfbd29c7c346b47b353ab4f89d2f451bee3
Lalit Sharma <38866385+Rattlecode17sh@users.noreply.github.com>
2020-04-24 22:03:39 +0530

    update details

---
+++ admin_area/function/details.php
@@ -0,0 +1,45 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<!DOCTYPE html>
+<html lang="en">
+<style>
+
+
+</style>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <!--Fontawesome CDN-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    
+</head>
+
+<body>
+    <div class="container">
+        <table class="table">
+            <br>
+            <thead>
+                <br>
+              <tr>
+                <th scope="col">RFID of Vehicle</th>
+                <th scope="col">Parking Time</th>
+                <th scope="col">Exiting Time</th>
+                <th scope="col">Cost</th>
+                <th scope="col">Lot No.</th>
+                <th scope="col">2 or 4 wheeler</th>
+              </tr>
+            </thead>
+            <tbody>
+              
+            </tbody>
+          </table>
+    </div>
+     
+
+  
+    
+</body>
