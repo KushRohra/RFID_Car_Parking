b518c05c8f1e324f811036bcae0f127e8e8742a1
Lalit Sharma <38866385+Rattlecode17sh@users.noreply.github.com>
2020-04-25 22:03:51 +0530

    page added

--- admin_area/function/parking_lot.php
+++ admin_area/function/parking_lot.php
@@ -32,7 +32,7 @@
 
     <div style="margin-top: 20px; margin-bottom: -15px;" class="container d-flex flex-column bd-highlight mb-6">
         <div class="btn-group-vertical">
-            <a href="../admin_dashboard.php"><button type="button" class="btn btn-primary">Go to Admin Dashboard</button></a>
+            <a href="../details.php"><button type="button" class="btn btn-primary">Go to Admin Dashboard</button></a>
         </div>        
       </div>
     <div class="container">
