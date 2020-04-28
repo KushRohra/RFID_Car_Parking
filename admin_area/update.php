96559841a4d70d19276157fac8206690cad5fc6d
Lalit Sharma <38866385+Rattlecode17sh@users.noreply.github.com>
2020-04-28 20:02:51 +0530

    Form fields added

--- admin_area/update.php
+++ admin_area/update.php
@@ -48,3 +48,35 @@
 
 
 ?>
+
+<!DOCTYPE html>
+ <html lang="en">
+   <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Change Password</title>
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!--Fontawesome CDN-->
+  	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+   </head>
+
+   <body>
+     <div class = "container">
+       <h1 class = "jumbotron text-center">VEHCILE ENTRY</h1>
+
+        <form>
+          <div class="form-group">
+
+            <label for="rfid">RFID of vehicle</label>
+            <input type="text" class="form-control" id="rfid"  placeholder="Enter RFID of vehicle " >
+
+          </div>
+          <div class="form-group">
+            <label for="typeofvehicle">Type of vehicle</label>
+            <input type="text" class="form-control" id="retypepassword" placeholder="Enter type of vehivle" >
+          </div>
+         
+        </form>
+    </div>
+   </body>
+ </html>
