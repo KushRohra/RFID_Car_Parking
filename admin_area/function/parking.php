b5f86382ca036fdbe5616eead5b8a0f821dff8ee
KushRohra <iec2017024@iiita.ac.in>
2020-04-28 20:57:58 +0530

    2 wheeler parking spaces added

--- admin_area/function/parking.php
+++ admin_area/function/parking.php
@@ -1,9 +1,30 @@
+<?php
+
+  session_start();
+  include("../../database/db.php");
+
+  $shop_name = $_SESSION['username'];
+  $shop_name = $shop_name."_2";
+  $query = "select * from $shop_name";
+  $two = 0;
+  $two_parked = 0;
+  $run_query = mysqli_query($conn, $query);
+  while($array = mysqli_fetch_array($run_query))
+  {
+      $two = $two + 1;
+      if($array['parked'] == 0)
+          $two_parked = $two_parked + 1;
+  }
+
+?>
+
+
 <!DOCTYPE html>
  <html lang="en">
    <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Change Password</title>
+     <title>Parking Lot Status</title>
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <!--Fontawesome CDN-->
   	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
@@ -26,8 +47,8 @@
           <tr>
             <th scope="row">1</th>
             <td>2 Wheeler</td>
-            <td></td>
-            <td></td>
+            <td><?php echo $two; ?></td>
+            <td><?php echo $two_parked; ?></td>
           </tr>
           <tr>
             <th scope="row">2</th>
